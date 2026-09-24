d151ce400b2cde493b10430ce0a99d9b666d095c
lgc2 <lgustavoccarvalho@gmail.com>
2026-09-24 20:56:09 -0300

    change 'Work in progress' page style

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
 function Home() {
-  return <h1>I want this to be published on the internet!!!</h1>;
+  return (
+    <main
+      style={{
+        alignItems: "center",
+        backgroundColor: "#111827",
+        color: "#f9fafb",
+        display: "flex",
+        fontFamily: "sans-serif",
+        justifyContent: "center",
+        minHeight: "100vh",
+        textAlign: "center",
+      }}
+    >
+      <h1>Work in progress!</h1>
+    </main>
+  );
 }
 
 export default Home;
